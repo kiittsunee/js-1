d4b966866f50c46ede5a6afdb286d3a25d737622
ann <annaocirova5@gmail.com>
2024-03-28 10:36:41 +0800

    Пример 2

--- Homework_1.js
+++ Homework_1.js
@@ -1,3 +1,9 @@
 const result = 5 > 4;
 console.log(result); // => true
 console.log('one' !== 'one'); // => false
+
+
+const isInfant = (age) => age < 1;
+
+isInfant(3); // false
+isInfant(0.5); // true
